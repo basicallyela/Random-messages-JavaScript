b57d3122fcf5205d655cf84df948198d295d2caa
Ela Baljo <ela.baljo@treca-gimnazija.edu.ba>
2023-01-01 17:42:49 +0100

    pocetak

--- VS Code kurs/script.js
+++ VS Code kurs/script.js
@@ -1,1 +1,10 @@
-console.log('hello')
+const messages = ['hey', 'girlie', 'bestie', 'tea']
+function giveMessages(msgs){
+    let lght = msgs.length
+    let h = Math.floor(Math.random() * lght)
+    console.log(msgs[h])
+}
+giveMessages(messages)
+/*for( let i=0; i<messages.length; i++){
+    console.log(messages[i])
+}*/
